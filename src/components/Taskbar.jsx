eb37a6bf45c282486057898b7575a57bbd3dc3c0
Semyon Tyo <78652129+aerosness@users.noreply.github.com>
2026-08-31 18:00:15 -0600

    Restore original taskbar icons

--- src/components/Taskbar.jsx
+++ src/components/Taskbar.jsx
@@ -7,6 +7,13 @@ const startMenuIconStyle = {
   height: '40px',
 };
 
+const classicTaskbarIcons = {
+  info: '/resources/ico/help.ico',
+  about: '/resources/ico/doc.ico',
+  projects: '/resources/ico/projects.ico',
+  links: '/resources/ico/folder.ico',
+};
+
 const leftStartMenuItems = [
   {
     id: 'info',
@@ -279,7 +286,7 @@ function Taskbar({
               <img
                 className="taskbaricon"
                 draggable="false"
-                src={windowData.icon}
+                src={classicTaskbarIcons[windowData.id] ?? windowData.icon}
                 alt=""
                 width="35"
                 height="35"
